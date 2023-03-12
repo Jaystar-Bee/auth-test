19b3c494d98b66ebebcd1592b0ebb3e8b5b0c9e8
I'm Jaystar <58359895+Jaystar-Bee@users.noreply.github.com>
2023-03-12 16:04:32 +0100

    created the boiler plate

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,35 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    token: null,
+    user: {},
   },
   getters: {
+    isAuthenticated(state) {
+      return !!state.token
+    }
   },
   mutations: {
+    setToken(state, payload) {
+      const token = payload.token
+      const user = payload.user
+
+      state.token = token
+      state.user = user
+    }
   },
   actions: {
+    loginUser(context, payload) {
+      setInterval(() => {
+        const expireTime = new Date().getTime() * 60 * 60 * 4
+        const token = "db382r38r3b3908e3r432fiebuqwebu-dbwufm"
+        const user = payload
+        localStorage.setItem("token", token)
+        localStorage.setItem("expireTime", expireTime)
+        context.commit("setToken", { user, token })
+      }, 500)
+
+    },
   },
   modules: {
   }
